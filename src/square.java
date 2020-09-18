7ff284d0a96b8aa38f74c4a70b384c655219cef2
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2020-09-18 13:45:43 -0400

    final commit

---
+++ src/square.java
@@ -0,0 +1,11 @@
+
+public class square {
+
+        public static void main(String[] args) {
+            System.out.println("H E L L O");
+            System.out.println("E       A");
+            System.out.println("L       P");
+            System.out.println("L       C");
+            System.out.println("O A P C S");
+        }
+}
