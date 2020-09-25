28fd9af00432a2524f27424a8555ce4a61575d49
Aidan Forrester <agregforrester@gmail.com>
2020-09-25 12:44:28 -0400

    exercise 3

---
+++ src/perimeter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+        public class perimeter {
+                public static void main(String[] args) {
+                    Scanner in = new Scanner(System.in);
+
+                    System.out.println("Height : ");
+                    double height = in.nextDouble();
+                    in.nextLine();
+
+                    System.out.println("Width : ");
+                    double width = in.nextDouble();
+
+                    double perimeter = (2 * height * 2.54) + (2 * width * 2.54);
+
+                    System.out.println(" ");
+                    System.out.printf("%,.2f", perimeter); System.out.println(" square centimeters");
+                    in.close();
+             }
+        }
